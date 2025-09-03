f60bc7583d38e323cbd9d13ac7a92d5f27153221
Baskar <119995743+Baskarbalakrishnan@users.noreply.github.com>
2025-09-03 06:35:15 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -25,15 +25,15 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
